342022c89a59d0d3ae074e5ac66047cadfefad21
tau2020 <makombemt@gmail.com>
2022-11-01 01:19:38 +0200

    added mdb carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,14 @@ import { DatediffServiceTsService } from './count-down/datediff.service.ts.servi
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  slides = [
+    { img: 'https://via.placeholder.com/600.png/09f/fff' },
+    { img: 'https://via.placeholder.com/600.png/021/fff' },
+    { img: 'https://via.placeholder.com/600.png/321/fff' },
+    { img: 'https://via.placeholder.com/600.png/422/fff' },
+    { img: 'https://via.placeholder.com/600.png/654/fff' },
+  ];
+  slideConfig = { slidesToShow: 4, slidesToScroll: 4 };
   public timeLeft$: Observable<TimeComponents>;
   constructor(
     private dateDiff: DatediffServiceTsService
@@ -18,5 +26,24 @@ export class AppComponent {
       map(x => dateDiff.calcDateDiff()),
       shareReplay(1)
     );
+  }
+
+  addSlide() {
+    this.slides.push({ img: 'http://placehold.it/350x150/777777' });
+  }
+  removeSlide() {
+    this.slides.length = this.slides.length - 1;
+  }
+  slickInit(e: any) {
+    console.log('slick initialized');
+  }
+  breakpoint(e: any) {
+    console.log('breakpoint');
+  }
+  afterChange(e: any) {
+    console.log('afterChange');
+  }
+  beforeChange(e: any) {
+    console.log('beforeChange');
   }
 }
